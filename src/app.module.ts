cbf88ad42c50f98f66b8c25d9eb0669c9d05df1f
gitihm <ismael897@outlook.com>
2019-01-17 17:47:17 +0700

    install nestjs & create file ItemsController

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { ItemsController  } from './items/items.controller';
+
+@Module({
+  imports: [],
+  controllers: [ItemsController ],
+  providers: [],
+})
+export class AppModule {}
